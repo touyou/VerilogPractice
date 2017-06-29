16fd2d80dde96c36fcaf6fc7b36913641640a565
touyou <fujiyou1121@gmail.com>
2017-06-29 15:56:38 +0900

    edit datamover_axi

--- axi_archive/datamover_axi.sv
+++ axi_archive/datamover_axi.sv
@@ -37,24 +37,65 @@ module datamover_axi
      input wire                       clk,
      input wire                       rstn);
 
-   assign iaddr = 0;
+   reg [6:0]                state;
+   reg [IAWIDTH-1:0]        pc;
+   wire [3:0]               opcode;
+   wire [AWIDTH-1:0]        op_a;
+   wire [AWIDTH-1:0]        op_b;
 
-   assign axi_awvalid = 0;
-   assign axi_awaddr = 0;
+   // Operand: 0x0 copy ELSE end exec
+   assign opcode = inst[4+AWIDTH+AWIDTH-1:AWIDTH+AWIDTH];
+   // Copy Source
+   assign op_a = inst[AWIDTH-1:0];
+   // Copy Destination
+   assign op_b = inst[AWIDTH+AWIDTH-1:AWIDTH];
+   assign iaddr = pc;
+
+   localparam s_wait        = 7'b0000001;
+   localparam s_inst_fetch  = 7'b0000010;
+   localparam s_inst_decode = 7'b0000100;
+   localparam s_d_load_req  = 7'b0001000;
+   localparam s_d_load_acp  = 7'b0010000;
+   localparam s_d_store_req = 7'b0100000;
+   localparam s_d_store_acp = 7'b1000000;
+   // address write channel output axi_awvalid, input axi_awready, output  axi_awaddr, output axi_awprot,
+   // data write channel output axi_wvalid. input axi_wready, output axi_wdata, output axi_wstrb,
+   // response channel input axi_bvalid, output axi_bready, input axi_bresp,
+   // address read channel output axi_arvalid, input axi_arready, output axi_araddr, output axi_arprot,
+   // read data channel input axi_rvalid, output axi_rready, input axi_rdata, input axi_rresp,
+
+   // Ignore this program
    assign axi_awprot = 0;
-   
-   assign axi_wvalid = 0;
-   assign axi_wdata = 0;
-   assign axi_wstrb = 0;
-
-   assign axi_bready = 0;
-   
-   assign axi_arvalid = 0;
-   assign axi_araddr = 0;
    assign axi_arprot = 0;
+   assign axi_wstrb = 4'b1111;
 
-   assign data_rdy = 0;
-   
+   always @(posedge clk) begin
+      if (~rstn) begin
+        state <= s_wait;
+        pc <= 0;
+        data_rdy <= 0;
+        axi_awvalid <= 0;
+        axi_awaddr <= 0;
+
+        axi_wvalid <= 0;
+        axi_wdata <= 0;
+
+        axi_bready <= 0;
+        // Status
+        axi_bresp <= 0;
+
+        axi_arvalid <= 0;
+        axi_araddr <= 0;
+
+        axi_rready <= 0;
+        // Status
+        axi_rresp <= 0;
+      end else begin
+        if (state == s_wait) begin
+        end
+      end
+   end
+
 endmodule
 
 `default_nettype wire
